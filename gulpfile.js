fd387dd505c36c2219229658f58af11447ad6731
Sander Knopper <sander@knopper-ict.nl>
2017-07-22 16:18:45 +0200

    Add instafeed.js

--- gulpfile.js
+++ gulpfile.js
@@ -61,6 +61,9 @@ gulp.task('copy', function() {
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
         .pipe(gulp.dest('vendor/jquery'))
 
+    gulp.src(['node_modules/instafeed.js/*.js'])
+	    .pipe(gulp.dest('vendor/instafeed'))
+
     gulp.src([
             'node_modules/font-awesome/**',
             '!node_modules/font-awesome/**/*.map',
